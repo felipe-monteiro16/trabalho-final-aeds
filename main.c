cd4e41cdcb7374112c1d14b06be70032121872db
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-11-28 20:28:22 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    printf(
+        "==============================\n"
+        "       SELECIONAR OPÇÃO\n"
+        "==============================\n"
+        "1 - ADICIONAR UM VALOR\n"
+        "2 - LISTAR VALORES\n"
+        "3 - REMOVER UM VALOR\n"
+        "");
+    return 0;
+}
+
