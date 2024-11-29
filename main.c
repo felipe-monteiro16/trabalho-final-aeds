6ff1ff483fda7c7c8b5675fb59e40050fc985234
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-11-29 18:03:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define maxpessoas 50
+/*PROFESSORES: Código (001, 029, ...), nome professor, Titulação (ESP, MST,
+DOU, POS), sexo (M,F); salário, Departamento (DCC, DEX, DCA, DLT, DEC, ...);*/
+struct teste{
+    char codigo[4];
+    char nome[6];
+    char titulacao[4];
+    char sexo[2];
+    int salario;
+    char departamento[4];
+};
 
 
-
-int main()
-{
+void main(){
     printf(
         "==============================\n"
         "       SELECIONAR OPÇÃO\n"
@@ -12,6 +22,12 @@ int main()
         "2 - LISTAR VALORES\n"
         "3 - REMOVER UM VALOR\n"
         "");
-    return 0;
+    
+    struct teste p[maxpessoas] = {"021", "João", "MST", "M", 12500, "DCC"};
+    printf("\nCódigo: %s", p[0]s1.codigo);
+    printf("\nNome: %s", s1.nome);
+    printf("\nTitulação: %s", s1.titulacao);
+    printf("\nSexo: %s", s1.sexo);
+    printf("\nSalário:  %d", s1.salario);
+    printf("\nDepartamento: %s", s1.departamento);
 }
-
