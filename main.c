813a7612ffca066f4faa6b3ba5d3c11b28b92a40
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-12-01 12:55:22 -0300

    Update main.c
    Adicionado:
    1 - INCLUSÃO DE DADOS
    2 - LISTAGEM GERAL DE DADOS
    
    Faltam:
    3 - LISTAGEM PARCIAL DE DADOS
    4 - MODIFICAÇÃO TOTAL DE DADOS
    5 - MODIFICAÇÃO PARCIAL DE DADOS

--- main.c
+++ main.c
@@ -1,33 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 #define maxpessoas 50
-/*PROFESSORES: Código (001, 029, ...), nome professor, Titulação (ESP, MST,
-DOU, POS), sexo (M,F); salário, Departamento (DCC, DEX, DCA, DLT, DEC, ...);*/
-struct teste{
-    char codigo[4];
-    char nome[6];
-    char titulacao[4];
-    char sexo[2];
-    int salario;
-    char departamento[4];
+
+/*PROFESSORES: 
+Código (001, 029, ...), 
+nome professor, 
+Titulação (ESP, MST, DOU, POS), 
+sexo (M,F); 
+salário, 
+Departamento (DCC, DEX, DCA, DLT, DEC, ...)
+*/
+struct pessoa{
+    char codigo[50];
+    char nome[50];
+    char titulacao[50];
+    char sexo[50];
+    //int salario;
+    char departamento[50];
 };
 
+int cont=-1;
+struct pessoa pg[maxpessoas];
 
-void main(){
-    printf(
-        "==============================\n"
-        "       SELECIONAR OPÇÃO\n"
-        "==============================\n"
-        "1 - ADICIONAR UM VALOR\n"
-        "2 - LISTAR VALORES\n"
-        "3 - REMOVER UM VALOR\n"
-        "");
-    
-    struct teste p[maxpessoas] = {"021", "João", "MST", "M", 12500, "DCC"};
-    printf("\nCódigo: %s", p[0]s1.codigo);
-    printf("\nNome: %s", s1.nome);
-    printf("\nTitulação: %s", s1.titulacao);
-    printf("\nSexo: %s", s1.sexo);
-    printf("\nSalário:  %d", s1.salario);
-    printf("\nDepartamento: %s", s1.departamento);
+
+void pausa() {
+    printf("\n\n===================================\n");
+    printf("Send any input to continue...");
+    char a;
+    scanf("%s", &a); // Aguarda a entrada do usuário
 }
+
+
+void adicionapessoa(){
+    cont++;
+    system("clear");
+    printf("===================================\n");
+    printf("ADICIONANDO PROFESSOR...\n");
+    printf("===================================\n");
+    printf("Código: ");
+    scanf("%s", pg[cont].codigo);
+    printf("Nome: ");
+    scanf("%s", pg[cont].nome);
+    printf("Titulação: ");
+    scanf("%s", pg[cont].titulacao);
+    printf("Sexo: ");
+    scanf("%s", pg[cont].sexo);
+    //printf("Salário: ");
+    //scanf("%d", &pg.salario);
+    printf("Departamento: ");
+    scanf("%s", pg[cont].departamento);
+    pausa();
+    return;
+}
+
+
+void listagemgeral(){
+    int c;
+    system("clear");
+    printf("===================================\n");
+    printf("LISTAGEM GERAL\n");
+    printf("===================================\n");
+    for(c=0; c<=cont; c++){
+        printf("\n-------------------------\n");
+        printf("Professor %d\n", c+1);
+        printf("-------------------------");
+        printf("\nCódigo: %s", pg[c].codigo);
+        printf("\nNome: %s", pg[c].nome);
+        printf("\nTitulação: %s", pg[c].titulacao);
+        printf("\nSexo: %s", pg[c].sexo);
+        //printf("\nSalário:  %d", &pg.salario);
+        printf("\nDepartamento: %s", pg[c].departamento);  
+        printf("\n");
+    }
+    pausa();
+    return;
+}
+
+
+int main(){
+    int num = 0;
+    setlocale(LC_ALL, "Portuguese_Brasil");
+    while (num != -1)
+    {
+        system("clear");
+        printf(
+        "===================================\n"
+        "          SELECIONAR OPÇÃO\n"
+        "===================================\n"
+        "1 - INCLUSÃO DE DADOS\n"
+        "2 - LISTAGEM GERAL DE DADOS\n"
+        "3 - LISTAGEM PARCIAL DE DADOS\n"
+        "4 - MODIFICAÇÃO TOTAL DE DADOS\n"
+        "5 - MODIFICAÇÃO PARCIAL DE DADOS\n"
+        "===================================\n");
+        printf("Sua Opção: ");
+        scanf("%d", &num);
+        if(num == 1)
+            adicionapessoa();
+        if(num == 2)
+            listagemgeral();
+    }
+    return 0;
+}
+
+
