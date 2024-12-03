ebc3b2692423963d6bfa3c29af41ffaadecbd8d6
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-12-03 18:04:01 -0300

    Update main.c
    Adicionado:
     - LISTAGEM PARCIAL DE DADOS
        * Seleção de Pessoa (completo)
        * Seleção de Categoria (incompleto)
    
    Resta:
    - MODIFICAÇÃO TOTAL DE DADOS
    - MODIFICAÇÃO PARCIAL DE DADOS

--- main.c
+++ main.c
@@ -21,7 +21,8 @@ struct pessoa{
     char departamento[50];
 };
 
-int cont=-1;
+
+int cont = 0;
 struct pessoa pg[maxpessoas];
 
 
@@ -35,6 +36,7 @@ void pausa() {
 
 void adicionapessoa(){
     cont++;
+    char nome1;
     system("clear");
     printf("===================================\n");
     printf("ADICIONANDO PROFESSOR...\n");
@@ -43,6 +45,7 @@ void adicionapessoa(){
     scanf("%s", pg[cont].codigo);
     printf("Nome: ");
     scanf("%s", pg[cont].nome);
+    // strcpy(nome1, pg[cont].nome);
     printf("Titulação: ");
     scanf("%s", pg[cont].titulacao);
     printf("Sexo: ");
@@ -79,28 +82,149 @@ void listagemgeral(){
 }
 
 
+void listagemparcial(){
+    int op, bk = 0;
+    while(bk == 0){     // OPÇÕES LISTAGEM PARCIAL
+        system("clear");
+        printf("===================================\n");
+        printf("LISTAGEM PARCIAL\n");
+        printf("===================================\n");
+        printf("TIPO DE LISTAGEM: \n");
+        printf("\n[0] - Filtrar por Categoria\n");
+        printf("[1] - Filtrar por Pessoa\n");
+        printf("[2] - Cancelar\n");
+        printf("\nSua Opção: ");
+        scanf("%d", &op);
+        switch(op){
+            case 0:     // FILTRAR POR CATEGORIA
+                int cat, c;
+                system("clear");
+                printf("===================================\n");
+                printf("LISTAGEM PARCIAL\n");
+                printf("===================================\n");
+                printf("Selecionar Categoria:\n\n");
+                printf("[0] - Código\n");
+                printf("[1] - Nome\n");
+                printf("[2] - Titulação\n");
+                printf("[3] - Sexo\n");
+                printf("[4] - Departamento\n");
+                printf("\n Sua Opção:");
+                scanf("%d", &cat);
+                switch(cat){
+                    case 0:     // CÓDIGO
+                        for(c=0; c<=cont; ++c){
+                            printf("\nProfessor %d\n", c+1);
+                            printf("Código: %s\n", pg[c].codigo);
+                            
+                        }
+                        break;
+                    case 1:     // NOME
+                    
+                        break;
+                    case 2:     // TITULAÇÃO
+                    
+                        break;
+                    case 3:     // SEXO
+                    
+                        break;
+                    case 4:     // DEPARTAMENTO
+                    
+                        break;
+                    default:
+                        printf("\nOpção Inválida!");
+                        pausa();
+                }
+                bk = 1;
+                break;
+            case 1:
+                int p;
+                system("clear");
+                printf("===================================\n");
+                printf("LISTAGEM PARCIAL\n");
+                printf("===================================\n");
+                printf("Selecionar Pessoa:\n");
+                for(c=0; c<=cont; ++c){
+                    printf("[%d] - %s %d\n", c, pg[c].nome, c+1);
+                }
+                while(1 == 1){
+                    printf("Sua Opção: ");
+                    scanf("%d", &p);
+                    if(p > cont || p < 0)
+                        printf("Opção Inválida\n");
+                    else{
+                        printf("\n-------------------------\n");
+                        printf("Professor %d\n", p+1);
+                        printf("-------------------------");
+                        printf("\nCódigo: %s", pg[p].codigo);
+                        printf("\nNome: %s", pg[p].nome);
+                        printf("\nTitulação: %s", pg[p].titulacao);
+                        printf("\nSexo: %s", pg[p].sexo);
+                        //printf("\nSalário:  %d", &pg.salario);
+                        printf("\nDepartamento: %s", pg[p].departamento);  
+                        printf("\n");
+                        break;
+                    }
+                }
+                bk = 1;
+                break;
+            case 2:
+                bk = 1;
+                break;
+            default:
+                printf("\nOpção Inválida");
+                break;
+        }
+        pausa();
+    }
+}
+
+
 int main(){
-    int num = 0;
     setlocale(LC_ALL, "Portuguese_Brasil");
-    while (num != -1)
+    int num = 0;
+    strcpy(pg->codigo, "001");
+    strcpy(pg->nome, "João Batista");
+    strcpy(pg->titulacao, "DOU");
+    strcpy(pg->departamento, "DCC");
+    strcpy(pg->sexo, "M");
+    //{"001", "JoãoBatista", "DOU", "M", "DCC"};
+    //pg[1] = {"029", "AntônioWilson", "DOU", "M", "DCE"};
+    while (num != 99)
     {
         system("clear");
         printf(
         "===================================\n"
         "          SELECIONAR OPÇÃO\n"
         "===================================\n"
-        "1 - INCLUSÃO DE DADOS\n"
-        "2 - LISTAGEM GERAL DE DADOS\n"
-        "3 - LISTAGEM PARCIAL DE DADOS\n"
-        "4 - MODIFICAÇÃO TOTAL DE DADOS\n"
-        "5 - MODIFICAÇÃO PARCIAL DE DADOS\n"
+        " 1 - INCLUSÃO DE DADOS\n"
+        " 2 - LISTAGEM GERAL DE DADOS\n"
+        " 3 - LISTAGEM PARCIAL DE DADOS\n"
+        " 4 - MODIFICAÇÃO TOTAL DE DADOS\n"
+        " 5 - MODIFICAÇÃO PARCIAL DE DADOS\n"
+        "99 - ENCERRAR PROGRAMA\n"
         "===================================\n");
         printf("Sua Opção: ");
         scanf("%d", &num);
-        if(num == 1)
-            adicionapessoa();
-        if(num == 2)
-            listagemgeral();
+        switch(num){
+            case 1:
+                adicionapessoa();
+                break;
+            case 2:
+                listagemgeral();
+                break;
+            case 3:
+                listagemparcial();
+                break;
+            case 99:
+                printf("===================================\n");
+                printf("        PROGRAMA ENCERRADO\n");
+                printf("===================================\n");
+                break;
+            default:
+                printf("Opção Inválida.");
+                pausa();
+                break;
+        }
     }
     return 0;
 }
